4f2d33f723d0ea89175f99e4d5e5a2d1f9224311
WesleyZeus <wesley159silva@gmail.com>
2025-02-04 11:15:38 -0300

    Primeiro commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    nome = "Lopes"
+    dados = {"profissao": "Professora","canal": "Profa. Alba Lopes"}
+    return render_template('index.html', nome='wesley', dados=dados)
+
+
+@app.route('/contato')
+def contato():
+  return render_template('contato.html')
